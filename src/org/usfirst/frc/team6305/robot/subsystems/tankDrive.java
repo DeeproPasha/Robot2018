48a3b422573e568ef05a804c3d82f92eafa5a76d
DeeproPasha <35978720+DeeproPasha@users.noreply.github.com>
2018-02-08 16:09:14 -0800

    Renamed it, changed it for accuracy

---
+++ src/org/usfirst/frc/team6305/robot/subsystems/tankDrive.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team6305.robot.subsystems;
+
+import org.usfirst.frc.team6305.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class tankDrive extends Subsystem {
+	
+	public static tankDrive instance = new tankDrive();
+	
+	Spark frontLeft = new Spark(RobotMap.frontLeft);
+	Spark frontRight = new Spark(RobotMap.frontRight);
+	Spark backLeft = new Spark(RobotMap.backLeft);
+	Spark backRight = new Spark(RobotMap.backRight);
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void drive(double leftSpeed, double rightSpeed) {
+    	frontRight.set(-leftSpeed);
+		backRight.set(-leftSpeed);
+		
+		frontLeft.set(-rightSpeed);
+		backLeft.set(-rightSpeed);    	
+    }
+    
+    public static tankDrive getInstance() {
+    	return instance;
+    }
+}
+
